e0cb9beb504235f79f71173b0493e7471d10de1a
Gar <gar@lonelydatu.com>
2019-08-28 12:28:21 -0400

    one

--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -1,18 +1,19 @@
 const tl = new TimelineMax()
-const list1 = ["a1", "a2", "a3", "a4" ]
-const list2 = ["a5", "a6", "a7", "a8" ]
+const list1 = ["a1", "a2" ]
+const list2 = ["a3", "a4" ]
 const listB = ["b1", "b2", "b3", "b4", "b5", "b6" ]
 
 
 
 
-
+TweenMax.to('.player_qb_ball', .9, {rotation:20, ease:Power1.easeInOut, repeat:11, yoyo:true})
+TweenMax.to('.player_qb_arm', 1, {rotation:-5, ease:Power2.easeInOut, repeat:11, yoyo:true})
 
 
 function sentence(list){
 	const tl_list = new TimelineMax()
 	list.map(item=>{
-		tl_list.set(`.${item}`, {opacity:1}, "+=.15")
+		tl_list.set(`.${item}`, {opacity:1}, "+=.2")
 	})	
 	return tl_list
 }
@@ -26,19 +27,29 @@ function start(){
 
 	tl.from(".logo", .3, {opacity:0}, "+=.2")
 
-	tl.to(".player_tackle", .3, {x:0, y:0}, "+=.5")
+	tl.to([".a1", ".a2", ".a3", ".a4", ".logo"], .2, {opacity:0}, "+=1.5")
 
-	tl.add('f2', "+=1")
+	tl.to(".player_tackle", .4, {x:0, y:0, ease:Power2.easeOut}, "+=.5")
+
+	tl.add('f2', '+=.4')
 	tl.set(".frame2", {opacity:1}, "f2")
-	tl.to(".frame1", .5, {y:"-=250", ease:Power1.easeOut}, "f2")
-	tl.from(".frame2", .5, {y:"+=250", ease:Power1.easeOut}, "f2")
+	tl.to(".frame1", .4, {opacity:0, ease:Power2.easeOut}, "f2")
+	// tl.from(".frame2", .9, {x:"+=250", ease:Power2.easeOut}, "f2")
 	
 	tl.add('f2_all', "-=.1")
-	tl.add(sentence(listB), 'f2_all')
-	// tl.from(".phone", .3, {y:"+=30", ease:Power2.easeOut}, "f2_all")
-	// tl.from(".phone_scribble", .3, {y:"+=30", ease:Power2.easeOut}, "f2_all+=.1")
+	tl.from(".phone", .3, {y:"+=100", ease:Power2.easeOut}, "f2_all")
+	tl.from(".phone_scribble", .5, {scale:0, ease:Power2.easeInOut}, 'f2_all+=.3')
 
-	tl.from([".cta", ".footer"], .2, {opacity:0}, "+=.6")
+
+	tl.to('.b', .2, {opacity:1})
+	tl.from('.cta_wager', .3, {opacity:0}, "+=.8")
+	tl.to('.cta_wager', .1, {opacity:0}, "+=1.5")
+	tl.from('.cta_odds', .3, {opacity:0}, "+=.3")
+	
+	
+	
+
+	tl.from([".footer"], .2, {opacity:0}, "+=.6")
 
 	
 
